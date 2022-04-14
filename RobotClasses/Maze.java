654fa4348fe69f3bbda4aed35cc2334d3afcb546
v4lp3n <juliajsson@hotmail.com>
2022-04-14 23:52:41 +0200

    First commit.

---
+++ RobotClasses/Maze.java
@@ -0,0 +1,189 @@
+package RobotClasses; /**
+ * Class: RobotClasses.Maze.
+ *
+ * Knows information regarding the Mazes structure, location of
+ * start and goal as well as length of rows and columns. Can give
+ * information whether it is possible or not to move to a given
+ * RobotClasses.Position.
+ */
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static java.util.Collections.frequency;
+
+public class Maze {
+    private int maxColumn;
+    private int maxRow;
+    private HashMap<Position, Character> maze;
+    private Position start;
+    private Position goal;
+
+    /**
+     * Constructor for a RobotClasses.Maze using a Scanner reading from an input-file.
+     * Every RobotClasses.Maze must have a start- and goal-RobotClasses.Position, otherwise a RobotClasses.Maze
+     * will not be constructed. The Mazes row numbers as well as the maximum
+     * amount of columns on a row is stored.
+     * @param s A scanner using an input-file.
+     */
+    public Maze(Scanner s) throws IOException {
+        try {
+            int row = 0;
+            int length;
+            String nextLine;
+
+            HashMap<Position, Character> h = new HashMap<>();
+
+            while (s.hasNextLine()) {
+                nextLine = s.nextLine();
+                length = nextLine.length();
+                if (length > this.maxColumn) {
+                    this.maxColumn = length;
+                }
+
+                for (int i = 0; i < length; i++) {
+                    char c = nextLine.charAt(i);
+                    Position p = new Position(i, row);
+                    h.put(p, c);
+
+                    if (c == 'S') this.start = p;
+                    if (c == 'G') this.goal = p;
+                }
+
+                ++row;
+            }
+            s.close();
+            this.maxRow = row;
+            this.maze = h;
+
+            if (getStart() == null || getGoal() == null) {
+                throw new NullPointerException();
+            }
+            if (frequency(maze.values(), 'S') > 1) {
+                System.out.println("To many start-positions in the RobotClasses.Maze, only 1 is allowed.");
+                throw new IOException();
+            }
+            if (frequency(maze.values(), 'G') > 1) {
+                System.out.println("To many goal-positions in the RobotClasses.Maze, only 1 is allowed.");
+                throw new IOException();
+            }
+
+        } catch (NullPointerException | IOException mazeConditions) {
+            throw new IOException();
+        }
+    }
+
+    /**
+     * Method to check if it is possible to move to given RobotClasses.Position. The RobotClasses.Position
+     * must exist within the bounds of the RobotClasses.Maze and cannot be a wall.
+     * @param p The given RobotClasses.Position.
+     * @return True if it is possible to move to the RobotClasses.Position, otherwise false.
+     */
+    public boolean isMovable (Position p) {
+        if (this.maze.containsKey(p)) {
+            char c = this.maze.get(p);
+
+            return switch (c) {
+                case ' ', 'G', 'S' -> true;
+                default -> false;
+            };
+        }
+        return false;
+    }
+
+    /**
+     * Method for checking if a given RobotClasses.Position is the goal-RobotClasses.Position
+     * of the RobotClasses.Maze.
+     * @param p The given RobotClasses.Position
+     * @return True if the RobotClasses.Position is the goal-RobotClasses.Position, otherwise false.
+     */
+    public boolean isGoal (Position p) {
+        return p.equals(this.goal);
+    }
+
+    /**
+     * Method to get the goal-RobotClasses.Position in the RobotClasses.Maze.
+     * @return The goal-RobotClasses.Position in the RobotClasses.Maze.
+     */
+    public Position getGoal () throws NullPointerException {
+        try {
+            if (goal == null) {
+                throw new NullPointerException();
+            } else {
+                return goal;
+            }
+        } catch (NullPointerException noGoal) {
+            System.out.println("Missing goal-position in RobotClasses.Maze.");
+            return null;
+        }
+    }
+
+    /**
+     * Method to get the start-RobotClasses.Position in the RobotClasses.Maze.
+     * @return The start-RobotClasses.Position in the RobotClasses.Maze.
+     */
+    public Position getStart () {
+        try {
+            if (start == null) {
+                throw new NullPointerException();
+            } else {
+                return start;
+            }
+        } catch (NullPointerException noStart) {
+            System.out.println("Missing start-position in RobotClasses.Maze.");
+            return null;
+        }
+    }
+
+    /**
+     * Method for getting the maximum number of columns in the RobotClasses.Maze.
+     * @return An integer value representing the number of columns.
+     */
+    public int getNumColumns () {
+        return this.maxColumn;
+    }
+
+    /**
+     * Method for getting the number of rows in the RobotClasses.Maze.
+     * @return An integer value representing the number of rows.
+     */
+    public int getNumRows () {
+        return this.maxRow;
+    }
+
+
+    /* --- Methods used for debugging --- */
+    public char getC (Position p) {
+        return maze.get(p);
+    }
+
+    /*
+     * Checks if iteration to the right from specific RobotClasses.Position is possible.
+     * @param p The given RobotClasses.Position.
+     * @return True if iteration to the right is possible, false if otherwise.
+     */
+    public boolean iterateRight (Position p) {
+        return maze.containsKey(p);
+    }
+
+    /*
+     * Help function used to print the maze.
+     */
+    public void printMaze () {
+        int r = 0, c = 0;
+
+        while (r < maxRow) {
+            Position p = new Position(c, r);
+            System.out.print(maze.get(p));
+
+            while (iterateRight(p.getPosToEast())) {
+                p = p.getPosToEast();
+                System.out.print(maze.get(p));
+            }
+
+            System.out.print('\n');
+            r++;
+        }
+    }
+
+}
---
+++ RobotClasses/Position.java
@@ -0,0 +1,92 @@
+package RobotClasses;
+
+/**
+ * Class: RobotClasses.Position.
+ *
+ * Knows x- and y-coordinates as well as character value
+ * for a specific position. Can return these values as
+ * well as give the positions to north, south, east and
+ * west.
+ */
+
+public class Position {
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for a RobotClasses.Position using two integer values to
+     * set the x- and y-coordinate.
+     * @param x x-coordinate
+     * @param y y-coordinate
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Get x-coordinate of a RobotClasses.Position.
+     * @return An integer value.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Get y-coordinate of a RobotClasses.Position.
+     * @return An integer value.
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Get the RobotClasses.Position to the south of given RobotClasses.Position.
+     * @return A RobotClasses.Position.
+     */
+    public Position getPosToSouth() { return new Position(this.x, this.y + 1); }
+
+    /**
+     * Get the RobotClasses.Position to the north of given RobotClasses.Position.
+     * @return A RobotClasses.Position.
+     */
+    public Position getPosToNorth() {
+        return new Position(this.x, this.y - 1);
+    }
+
+    /**
+     * Get the RobotClasses.Position to the east of given RobotClasses.Position.
+     * @return A RobotClasses.Position.
+     */
+    public Position getPosToEast() {
+        return new Position(this.x + 1, this.y);
+    }
+
+    /**
+     * Get the RobotClasses.Position to the east of given RobotClasses.Position.
+     * @return A RobotClasses.Position.
+     */
+    public Position getPosToWest() {
+        return new Position(this.x - 1, this.y);
+    }
+
+    /**
+     * Checks if to Positions are equal based on x- and y-coordinates.
+     * @param o A RobotClasses.Position.
+     * @return True if equal, or false if else.
+     */
+    public boolean equals(Object o) {
+        Position p = (Position) o;
+        return this.getX() == p.getX() && this.getY() == p.getY();
+    }
+
+    /**
+     * Creates a hashcode using the Positions x- and y-coordinates.
+     * @return An integer value.
+     */
+    public int hashCode () {
+        return ((x + y) * (x + y + 1) / 2 + x);
+    }
+}
+
+
